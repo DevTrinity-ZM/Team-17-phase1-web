86dcd18de73b80d417c3d802d8170baacdf14ede
Ben Kazadi <kazadiben43@gmail.com>
2026-02-27 20:47:18 +0200

    Added details to the goal details page and linked goals to a json file using express

--- server/server.js
+++ server/server.js
@@ -1,15 +1,40 @@
-const express = require('express');
-const cors = require("corss");
+import express from "express";
+import { readFileSync, writeFileSync } from "fs";
+import cors from "cors";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
+app.use(express.json());
+app.use(cors());
+
+app.get("/goals", (req, res) => {
+  	const data = readFileSync("goals.json", "utf-8");
+  	res.json(JSON.parse(data));
 });
 
-const PORT = 5000;
+app.post("/goals", (req, res) => {
+	const data = readFileSync("goals.json", "utf-8");
+	const goals = JSON.parse(data);
+
+	goals.push(req.body);
+
+	writeFileSync("goals.json", JSON.stringify(goals, null, 2));
+
+	res.json({ message: "Goal added" });
+});
+
+app.delete("/goals/:id", (req, res) => {
+	const data = readFileSync("goals.json", "utf-8");
+	let goals = JSON.parse(data);
+
+	goals = goals.filter(goal => goal.id != req.params.id);
+
+	writeFileSync("goals.json", JSON.stringify(goals, null, 2));
+
+	res.json({ message: "Deleted" });
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port 3001");
 });
